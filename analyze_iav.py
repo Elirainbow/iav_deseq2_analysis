68e095f012070e080e783241b65cc223847b441a
Elirainbow <elizabeth12.jimenez07@gmail.com>
2026-06-04 20:20:34 -0600

    feat: clasificar genes significativos como upregulated o downregulated

--- analyze_iav.py
+++ analyze_iav.py
@@ -10,3 +10,22 @@ def is_significant(log2FoldChange, padj, log2fc_threshold, padj_threshold):
 print(is_significant(4.2, 0.0001, 1, 0.05))
 print(is_significant(0.3, 0.0001, 1, 0.05))
 print(is_significant(3.0, 0.8, 1, 0.05))
+
+
+# Responsabilidad: Clasificar un gen como "upregulated", "downregulated" o "not_significant" según su log2_fold_change y padj.
+# Entrada: log2_fold_change
+# Salida:"upregulated" o "downregulated", "not_significant"
+def classify_gene(log2FoldChange):
+    if log2FoldChange > 0:
+        return "upregulated"
+    elif log2FoldChange < 0:
+        return "downregulated"
+    else:
+        return "not_significant"
+
+
+print(classify_gene(4.2))
+# "upregulated"
+
+print(classify_gene(-3.0))
+# "downregulated"
